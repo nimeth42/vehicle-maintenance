ea6300010fc76b8feaba4b973875a3a46eae7aec
ThewmikaRandinu <randinusilva648@gmail.com>
2024-03-03 15:27:07 +0530

    create user login

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -45,4 +45,55 @@ exports.driverRegister = (req, res, next) => {
   });
 };
 
+exports.driverLogin = (req, res, next) => {
+  const { plateNo, password } = req.body;
 
+  // Find the user in the database based on the provided plateNo
+  User.findOne({ plateNo: plateNo })
+    .then(user => {
+      // If user not found, return failed status
+      if (!user) {
+        return res.status(401).json({
+          status: "failed",
+          comment: "User not found",
+          data: null,
+        });
+      }
+
+      // Compare the provided password with the hashed password from the database
+      bcrypt.compare(password, user.password, function(err, result) {
+        if (err) {
+          console.error('Error comparing passwords:', err);
+          return res.status(500).json({
+            status: "failed",
+            comment: "Internal server error",
+            data: null,
+          });
+        }
+
+        // If passwords match, return success status
+        if (result) {
+          return res.status(200).json({
+            status: "success from login",
+            comment: "success",
+            data: null,
+          });
+        } else {
+          // If password doesn't match, return failed status
+          return res.status(401).json({
+            status: "failed",
+            comment: "Incorrect password",
+            data: null,
+          });
+        }
+      });
+    })
+    .catch(error => {
+      console.error('Error finding user:', error);
+      return res.status(500).json({
+        status: "failed",
+        comment: "Internal server error",
+        data: null,
+      });
+    });
+};
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -5,3 +5,5 @@ const userController = require("../controller/user");
 route.post("/register", userController.driverRegister);
 route.post("/login", userController.driverLogin);
 module.exports = route;
+
+
