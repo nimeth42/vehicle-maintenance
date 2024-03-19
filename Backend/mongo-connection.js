0ea4d990541cfb8090c5fe9228292a936c80d918
Nimeth Fernando <nimeth42@gmail.com>
2024-03-19 23:01:02 +0530

    Update DB

--- Backend/mongo-connection.js
+++ Backend/mongo-connection.js
@@ -4,7 +4,7 @@ const connect = () => {
   mongoose
     .connect("mongodb+srv://devops663:cS3VonfdF2PYkn1O@cluster0.tdev0vt.mongodb.net/vehicle_maintance?retryWrites=true&w=majority")
     .then(() => {
-      console.log("Connected mongo to database-auth");
+      console.log("Connected mongo to Database");
     })
     .catch(() => {
       console.log("Connection failed!");
