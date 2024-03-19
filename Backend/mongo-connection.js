50ca2610619280070fcee4f6e17e3ad77e677659
Nimeth Fernando <nimeth42@gmail.com>
2024-03-19 22:33:58 +0530

    Update DB

--- Backend/mongo-connection.js
+++ Backend/mongo-connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb+srv://devops663:cS3VonfdF2PYkn1O@cluster0.tdev0vt.mongodb.net/?retryWrites=true&w=majority")
+    .connect("mongodb+srv://devops663:cS3VonfdF2PYkn1O@cluster0.tdev0vt.mongodb.net/vehicle_maintance?retryWrites=true&w=majority")
     .then(() => {
       console.log("Connected mongo to database-auth");
     })
