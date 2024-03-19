4db6416317c23d95c32bb57500e2af0138dcda02
Nimeth Fernando <nimeth42@gmail.com>
2024-03-19 21:59:42 +0530

    Update DB

--- Backend/mongo-connection.js
+++ Backend/mongo-connection.js
@@ -12,7 +12,7 @@ const connect = () => {
   //       console.log("Connection failed!");
   //     });
   mongoose
-    .connect("mongodb+srv://devops663:cS3VonfdF2PYkn1O@cluster0.tdev0vt.mongodb.net/?retryWrites=true&w=majority")
+    .connect("mongodb+srv://devops663:cS3VonfdF2PYkn1O@cluster0.tdev0vt.mongodb.net/vehicle_maintance?retryWrites=true&w=majority")
     .then(() => {
       console.log("Connected mongo to database-auth");
     })
--- baseUrl/baseUrl.js
+++ baseUrl/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = 'http://192.168.1.4:3000';
+const baseUrl = 'http://3.144.155.195:3000';
 export default baseUrl;
