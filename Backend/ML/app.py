d9b1aa8295f5db47678c71fcfe126a5de64eeca8
Nimeth Fernando <nimeth42@gmail.com>
2024-03-23 14:15:45 +0530

    Update port

--- Backend/ML/app.py
+++ Backend/ML/app.py
@@ -103,4 +103,4 @@ def predict():
     return jsonify({"class": result_class})
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
